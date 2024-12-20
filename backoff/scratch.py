ef0c0aa19cf6efb19b4e6d299180e23f5b1a6e38
BlakeJC94 <blakejamescook@gmail.com>
2024-12-20 15:50:58 +1100

    Exponential backoff

--- backoff/scratch.py
+++ backoff/scratch.py
@@ -1,4 +1,10 @@
 import time
+import random
+
+
+def try_with_random_exp_backoff(max_tries, a=0, b=1, c=2):
+    delays = [a + b * (random.random() * c ** (i + 1)) for i in range(max_tries)]
+    return try_with_backoff(delays)
 
 
 def try_with_backoff(delays):
@@ -19,14 +25,17 @@ def try_with_backoff(delays):
             if try_again:
                 raise last_exception
             return result
+
         return wrapped_func
+
     return wrapper
 
 
 foo = None
 
 
-@try_with_backoff([1,1,1,1,1])
+# @try_with_backoff([1, 1, 1, 1, 1])
+@try_with_random_exp_backoff(5)
 def function_that_fails_first_n_calls(n):
     global foo
     foo = foo or 0
@@ -41,6 +50,7 @@ def function_that_fails_first_n_calls(n):
 
 def main():
     print(function_that_fails_first_n_calls(2))
+
 
 if __name__ == "__main__":
     main()
